dbed1e3e371c03bf874829348f2bcc1c9bdaf473
yuchild <yuchild@gmail.com>
2025-06-24 22:03:43 -0700

    Updated 2025-06-24

--- app.py
+++ app.py
@@ -41,8 +41,8 @@ def main():
     - Last Price: Last price of the stock
     - Max Loss Amount: Maximum loss amount based on account balance and max loss percentage
     - Shares to Buy: Number of shares to buy based on risk amount and last price
-    - Stop Loss Price: Price to set for stop loss based on last price and max loss amount
-    - Take Profit Price: Price to set for take profit based on last price and 2x max loss amount above base price
+    - Stop Loss Price: Price to set for stop loss based on entry (last) price and max loss amount
+    - Take Profit Price: Price to set for take profit based on last price and 2x max loss amount above entry price
     """)
 
     # Determine start date based on interval
